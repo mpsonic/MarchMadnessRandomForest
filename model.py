505a9de3713afb728f3ec34c543dd29216dac661
mpsonic <mitch_paull@yahoo.com>
2017-03-11 09:18:18 -0600

    model trained and validated

--- model.py
+++ model.py
@@ -3,24 +3,28 @@ from loadTourneyData import loadTourneyData
 from loadRatingData import loadRatingData
 from sklearn.ensemble import RandomForestClassifier
 import random
+import numpy as np
 
 teams = loadTeamData()
 tourneyResults = loadTourneyData()
 ratings = {}
-for i in range(2013, 2016):
+for i in range(2013, 2018):
     ratings[i] = loadRatingData(i);
 
 def compareTeamStats(season, teamid1, teamid2):
+    # print ("team1: %s, team2: %s" % (teamid1, teamid2))
     r1 = ratings[season][teamid1]
     r2 = ratings[season][teamid2]
     comparison = {
-        "power":    r1["power"]     - r2["power"],
-        "offense":  r1["offense"]   - r2["offense"],
-        "defense":  r1["defense"]   - r2["defense"],
-        "hfa":      r1["hfa"]       - r2["hfa"],
-        "sos":      r1["sos"]       - r2["sos"],
-        "od":       r1["offense"]   - r2["defense"],
-        "do":       r1["defense"]   - r2["offense"]
+        "rating":   r1["rating"]    -   r2["rating"],
+        "winPct":   r1["winPct"]    /   r2["winPct"],
+        "power":    r1["power"]     -   r2["power"],
+        "offense":  r1["offense"]   -   r2["offense"],
+        "defense":  r1["defense"]   -   r2["defense"],
+        "hfa":      r1["hfa"]       -   r2["hfa"],
+        "sos":      r1["sos"]       -   r2["sos"],
+        "od":       r1["offense"]   -   r2["defense"],
+        "do":       r1["defense"]   -   r2["offense"]
     }
     return comparison.values()
 
@@ -40,12 +44,28 @@ def prepareTrainingData(season):
         features.append(comparison)
     return {"features": features, "targets": targets}
 
-def trainModel():
-    data = prepareTrainingData(2013)
-    randomForest = RandomForestClassifier(n_estimators=10, n_jobs=2)
+def test2017data():
+    r = ratings[2013]
+    teamIds = []
+    for teamid in r:
+        teamIds.append(teamid)
+    return compareTeamStats(2013, teamIds[0], teamIds[1])
+
+def trainModel(season):
+    data = prepareTrainingData(season)
+    randomForest = RandomForestClassifier(n_estimators=200, n_jobs=2)
     randomForest.fit(data["features"], data["targets"])
     return randomForest
 
+def testModel(rfModel, season):
+    data = prepareTrainingData(season)
+    return rfModel.score(data["features"], data["targets"])
+
 if __name__ == "__main__":
     print "training model"
-    trainModel()
+    rf = trainModel(2013)
+    print testModel(rf, 2014)
+    print rf.feature_importances_
+    # X = test2017data()
+    # X = np.asarray(X).reshape(1, -1)
+    # print(rf.predict_proba(X))
